3d38ba1e3ec58eb86b6a072a6edfc5240284652e
harrison-code-wilkinson <coleinbrentwood@gmail.com>
2022-07-04 18:11:13 -0500

    changes

--- wait_config.js
+++ wait_config.js
@@ -1,4 +1,4 @@
-export const WaitAddress = '0x17cdec08794b135D5B3080c31F75AAE1b4733f9a'
+export const WaitAddress = '0x8DF5F59302fD9F67598a051D8A9C9A15047FAc75'
 
 // Get rid of input box
 // button to connect to metamask
