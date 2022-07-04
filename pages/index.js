c7f665831b633ebfbfda1dd8b44ac293a84f6b70
harrison-code-wilkinson <coleinbrentwood@gmail.com>
2022-07-04 14:56:32 -0500

    broke some shit

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,6 @@ import { WaitAddress } from '../wait_config'
 export default function Home() {
 
 
-	let contract;
 	const [loadingState, setLoadingState] = useState('not-loaded');
 
 	const [sacs,setSacs] = useState([])
@@ -34,19 +33,37 @@ export default function Home() {
 
 
 	useEffect( ()=>{
-		init();
+
+
 		window.ethereum.on('accountsChanged', function (accounts) {
 		
+			
 
 			
 
 	
 		  }) 
 
+		
+
 
 
 
 	}, []);
+
+
+	async function connect(){ 
+		console.log("rbo")
+		if (window.ethereum) {
+
+		console.log("aht the shit")
+ 
+		await window.ethereum.request({ method: "eth_requestAccounts" });
+		window.web3 = new Web3(window.ethereum);
+		
+	   } else {
+		console.log("No wallet");
+	   }}
 
 
 
@@ -60,8 +77,10 @@ export default function Home() {
 
 		try{
 
+			
 			const web3Modal = new Web3Modal()
 			const connection = await web3Modal.connect()
+			console.log('fucking shit',connection)
 			const provider = new ethers.providers.Web3Provider(connection);
 			const signer = provider.getSigner();
 			const WaitContract = new ethers.Contract(WaitAddress, Wait.abi, signer);
@@ -253,8 +272,11 @@ export default function Home() {
 	
 	}
 
+	function fuckyou(){
+		console.log("shit")
+	}
+
 	async function mintSpecific(sac) {
-		console.log(sac)
 		const web3Modal = new Web3Modal();
 		const connection = await web3Modal.connect();
 		const provider = new ethers.providers.Web3Provider(connection);
@@ -276,11 +298,157 @@ export default function Home() {
 				(
 				<>	
 				<div>
-						<div className='w-screen md:h-32 h-80 bg-[#252E3F] flex items-center justify-center md:flex-row flex-col'>
-							<div className='h-24 w-24 '>
-								<img src='WAIT.png'></img>
+						<div className='w-full md:h-32 h-80 bg-[#252E3F] flex items-center justify-between md:flex-row flex-col'>
+							<div className='flex justify-center items-center md:flex-row flex-col md:ml-24'>
+								<div className='h-24 w-24 '>
+									<img src='WAIT.png'></img>
+								</div>
+								<h1 className='text-white text-6xl font-semibold mx-12'>$WAIT</h1> 
 							</div>
-							<h1 className='text-white text-6xl font-semibold mx-12'>$WAIT</h1> 
+							<div>
+								<button onClick={fuckyou} className='rounded-full w-48 h-16 bg-blue-400 md:mr-24' >Connect Wallet</button>
+							</div>
+							<button onClick={fuckyou}>flkdsjf;ldsaj</button>
+							
+						</div>
+
+						<div className='w-full bg-[#00e7fa] flex justify-center items-center '>
+							<a className=" text-[#252e3f] text-2xl font-sans font-bold mx-20 my-6 text-center">Claim your $WAIT now, or maybe wait a little longer!</a>
+						</div>
+
+						{checked?
+						<div className='w-full flex justify-center my-6'>
+							<h1 className='text-2xl font-semibold text-center'>For Address {addr}</h1>
+						</div>
+						:
+						<div className='w-full bg-white items-center mt-6'>
+							<form className='flex flex-col justify-center items-center' onSubmit={connect}>
+
+								<button type='submit'  className='md:w-5/12 w-3/5 h-12 md:text-xl text-lg text-white text-center bg-[#324dff] border-4 border-black my-5'>Check Database!</button>
+
+							</form>
+						
+						</div>
+
+						}
+						
+						<div className='flex justify-center'>
+						{total>0
+						?
+							<button onClick={mintAll}  className='md:w-5/12 w-3/5 h-12 md:text-xl text-lg text-white text-center bg-[#324dff] border-4 border-black my-5'>Claim {total} $WAIT!</button>
+						:
+							<>
+							</>
+						}
+						</div>
+
+				
+				<div className='flex justify-between lg:flex-col' key={id}>
+				<div className='flex justify-between lg:flex-row flex-col w-1/2 lg:w-full'>
+					{first.map((row,i) => (
+						<div key={i} className='lg:w-1/4 w-full flex items-center flex-col'>
+							<div className='w-full flex flex-col items-center'>
+								<h1 className='text-3xl font-semibold text-center'>{row.name}</h1>
+
+								<img className='w-48 h-48 p-5' src={row.image}></img>
+								
+								{checked?
+								<div className='w-full px-3'>
+								{row.inD==false
+								?
+									<h1 className='w-full bg-red-300 text-center text-3xl border-2 border-black p-4'>Not Eligible to Claim</h1>
+								:
+									<div>
+										{row.C==false?
+										<button className='w-full  bg-green-100 text-center text-3xl border-2 border-black p-4' onClick={() => {mintSpecific(row.id)}}> Claim {row.cla} $WAIT</button>
+										:
+										<h1 className='w-full  bg-yellow-100 text-center text-3xl border-2 border-black p-4'> You've already claimed!</h1>
+										}
+									</div>
+								}
+							</div>
+								:
+								<div className='w-full px-2' onClick={() => {mintSpecific(row.id)}}>
+									<h1 className='w-full  text-center text-3xl border-2 border-black p-4'> Check Address</h1>
+								</div>
+								}
+							</div>
+						</div>
+					))}
+				</div> 
+					
+				<div className='flex lg:justify-between lg:flex-row flex-col w-1/2 lg:w-full'>
+					{end.map((row,i) => (
+						<div key={i} className='lg:w-1/4 w-full flex items-center flex-col'>
+							<div className='lg:mt-12 w-full flex flex-col items-center'>
+								<h1 className='text-3xl font-semibold text-center'>{row.name}</h1>
+
+								<img className='w-48 h-48 p-5' src={row.image}></img>
+								{checked?
+								<div className='w-full px-3'>
+								{row.inD==false
+								?
+									<h1 className='w-full bg-red-300 text-center text-3xl border-2 border-black p-4'>Not Eligible to Claim</h1>
+								:
+									<div>
+										{row.C==false?
+										<button className='w-full  bg-green-100 text-center text-3xl border-2 border-black p-4' onClick={() => {mintSpecific(row.id)}}> Claim {row.cla} $WAIT</button>
+										:
+										<h1 className='w-full  bg-yellow-100 text-center text-3xl border-2 border-black p-4'> You've already claimed!</h1>
+										}
+									</div>
+								}
+							</div>
+								:
+								<div className='w-full px-2' onClick={() => {mintSpecific(row.id)}}>
+									<h1 className='w-full  text-center text-3xl border-2 border-black p-4'> Check Database</h1>
+								</div>
+								}
+
+								
+							</div>
+						</div>
+					))}
+				</div>		
+			</div>
+				
+				
+					
+
+					<hr className='mt-10  border-black'></hr>
+	
+					<div className='h-52 w-full flex flex-col items-center justify-center'>
+						<h1 className='text-2xl font-bold text-center'>Coast- a #pulsechain development company</h1>
+						<div className='flex justify-center'>
+							<a href='https://twitter.com/0xCoast'><img src='twit.png'  className='h-16 w-16 '></img></a>
+
+							<img src='wifi.png' className='h-16 w-16 p-1'></img>
+						</div>
+					</div>
+						
+					<div className='flex justify-center'>
+						<hr className='w-5/6 border-black mb-5'></hr>
+					</div>
+
+					<div className='w-full flex items-center justify-center mt-12'>
+						<p className='w-4/5 text-center leading-loose'>No part of content produced by 0xWait may be redistributed without express written permission from 0xCoast. This content is for educational and informational purposes only and should not constitute investment advice or an offer to sell or the solicitation of an offer to purchase any products or services. This information is not intended for any persons who are prohibited from receiving such information under the laws applicable to their place of citizenship, domicile or residence. © All rights reserved 0xCoast.</p>
+					</div>
+				</div>	
+				</>
+				):(
+					<>	
+				<div>
+						<div className='w-full md:h-32 h-80 bg-[#252E3F] flex items-center justify-between md:flex-row flex-col'>
+							<div className='flex justify-center items-center md:flex-row flex-col md:ml-24'>
+								<div className='h-24 w-24 '>
+									<img src='WAIT.png'></img>
+								</div>
+								<h1 className='text-white text-6xl font-semibold mx-12'>$WAIT</h1> 
+							</div>
+							<div className=''>
+								<button className='rounded-full w-48 h-16 bg-blue-400 md:mr-24'>Connect Wallet</button>
+							</div>
+							
 						</div>
 
 						<div className='w-full bg-[#00e7fa] flex justify-center items-center '>
@@ -406,10 +574,6 @@ export default function Home() {
 					</div>
 				</div>	
 				</>
-				):(
-					<p>
-						Loading...
-					</p>
 				)}
 			</div>
 		</>
