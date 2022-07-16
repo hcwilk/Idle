da8014a2aebfbf58ee54412f21ac59fccd5e58e9
harrison-code-wilkinson <coleinbrentwood@gmail.com>
2022-07-16 13:51:48 -0500

    changed to main address

--- pages/index.js
+++ pages/index.js
@@ -40,6 +40,7 @@ export default function Home() {
 
 
 	  useEffect(()=>  {
+		console.log(WaitAddress)
 		// setShowModal(true)
 		baseInit()
 		  if(window.ethereum){
--- wait_config.js
+++ wait_config.js
@@ -1,4 +1,4 @@
-export const WaitAddress = '0xbBff0CFEC73AFB4e0056bbd69D1C798667686d72'
+export const WaitAddress = '0xb5588C411ba0bb7D38865fdC51D082d004e519F7'
 
 // Get rid of input box
 // button to connect to metamask
